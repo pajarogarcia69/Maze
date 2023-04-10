05ec001c02023f87637ed4a5e6ad2350615b3c40
pajarogarcia69 <stevenpajarogarcia@gmail.com>
2023-04-09 21:46:43 -0500

    Se hace el recorrido del camino del laberinto se le añade un mensaje cuando se puede salir del laberinto y cuando no.

--- Maze.Logic/Maze.cs
+++ Maze.Logic/Maze.cs
@@ -30,10 +30,11 @@
             return output;
         }
 
-        private void FillMaze()
+        public void FillMaze()
         {
             FillBorders();
             FillObstacles();
+            Path();
         }
 
         private void FillObstacles()
@@ -59,24 +60,24 @@
         {
             for (int i = 0; i < N; i++)
             {
-                _maze[0, i] = '█';
+                _maze[0, i] = '═';
             }
             for (int i = 0; i < N - 1; i++)
             {
                 _maze[1, i] = ' ';
             }
 
-            _maze[1, N - 1] = '█';
+            _maze[1, N - 1] = '║';
             for (int i = 2; i < N - 2; i++)
             {
-                _maze[i, 0] = '█';
+                _maze[i, 0] = '║';
                 for (int j = 1; j < N - 1; j++)
                 {
                     _maze[i, j] = ' ';
                 }
-                _maze[i, N - 1] = '█';
+                _maze[i, N - 1] = '║';
             }
-            _maze[N - 2, 0] = '█';
+            _maze[N - 2, 0] = '║';
             for (int i = 1; i < N; i++)
             {
                 _maze[N - 2, i] = ' ';
@@ -84,7 +85,57 @@
 
             for (int i = 0; i < N; i++)
             {
-                _maze[N - 1, i] = '█';
+                _maze[N - 1, i] = '═';
+            }
+        }
+
+        private void Path()
+        {
+            for(int i = 1; i < N-1;i++) 
+            {
+                for(int j = 1;j < N;j++)
+                {
+                    if ((_maze[i,j] != '█') && (_maze[i, j] != '║'))
+                    {
+                        _maze[i, j] = '→';
+                    }
+                    else
+                    {
+                        if ((_maze[i+1,j-1] != '█') && (_maze[i + 1, j - 1] != '║'))
+                        {
+                            _maze[i + 1, j - 1] = '↓';
+                            i ++;
+                            j--;
+                        }
+                        else
+                        {
+                            if ((_maze[i, j - 2] != '█') && (_maze[i, j - 2] != '║'))
+                            {
+                                _maze[i, j - 2] = '←';
+                                i ++;
+                                j -= 2;
+                            }
+                            else
+                            {
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool CouldGetOut()
+        {
+            var i = N;
+            var j = N;
+            if (_maze[i - 2, j - 1] == '→' || _maze[i - 2, j - 1] == '↓' || _maze[i - 2, j - 1] == '←')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
--- Maze.UI/Program.cs
+++ Maze.UI/Program.cs
@@ -1,8 +1,35 @@
 ﻿using Maze.Logic;
 
 Console.BackgroundColor = ConsoleColor.Blue;
-Console.ForegroundColor = ConsoleColor.Yellow;
-var maze = new MyMaze(30, 100);
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("══════════════════════════════");
+Console.WriteLine("║         M A Z E            ║");
+Console.WriteLine("══════════════════════════════");
+Console.WriteLine();
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+var maze = new MyMaze(30, 50);
 Console.Write(maze);
 Console.ForegroundColor = ConsoleColor.White;
 Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine();
+
+var vorf = maze.CouldGetOut();
+if (vorf)
+{
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("¡ Se pudo salir del laberinto !");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Red; ;
+    Console.WriteLine("¡ No se pudo llegar al final del laberinto !");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.WriteLine();
+}
