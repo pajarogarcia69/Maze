cf7fc92f4c1b3dbcafbc7a9009aa1c6a81dc4da3
XZEUS <XZEUS@XZEUS>
2023-04-09 19:21:28 -0500

    Add project files.

---
+++ Maze.UI/Program.cs
@@ -0,0 +1,8 @@
+﻿using Maze.Logic;
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Yellow;
+var maze = new MyMaze(30, 100);
+Console.Write(maze);
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
